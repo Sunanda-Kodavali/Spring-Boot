d399045bf1c02083aec72bbb297a91a539606754
Sunanda-Kodavali <sunanda.kodavali@gmail.com>
2024-10-15 14:12:13 +0200

    directing from one html to another with thymeleaf

--- f2-forms/src/main/java/dci/j24e01/f2_forms/AppController.java
+++ f2-forms/src/main/java/dci/j24e01/f2_forms/AppController.java
@@ -1,7 +1,10 @@
 package dci.j24e01.f2_forms;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
@@ -12,7 +15,19 @@ public class AppController {
     }
 
     @GetMapping("/contact")
-    public String contact() {
+    public String contact(Model model) {
+        model.addAttribute("welcomeMessage","hello contact");
         return "contact";
+    }
+
+    @GetMapping("/question")
+    public String question() {
+        return "question";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam String name, Model model) {
+        model.addAttribute("name",name);
+        return "greeting";
     }
 }
