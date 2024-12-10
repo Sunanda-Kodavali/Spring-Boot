99d2c1e2008f7b3618b43c5fe17bfb37ec8a02d8
Sunanda-Kodavali <sunanda.kodavali@gmail.com>
2024-12-10 09:44:17 +0100

    without Repo annotation

--- SpringDataREST/src/main/java/com/example/SpringDataREST/EmployeeController.java
+++ SpringDataREST/src/main/java/com/example/SpringDataREST/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.SpringDataREST;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,12 +14,12 @@ public class EmployeeController
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
         return "index";
     }
 
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public String employees(Model model) {
         List<Employee> employees = employeeRepository.findAll();
         model.addAttribute("employees", employees);
--- SpringDataREST/src/main/java/com/example/SpringDataREST/EmployeeRepository.java
+++ SpringDataREST/src/main/java/com/example/SpringDataREST/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.example.SpringDataREST;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "employees")
+import java.util.List;
+
+/*@RepositoryRestResource(path = "employees")*/
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    List<Employee> findByLastName(String lastName);
 }
